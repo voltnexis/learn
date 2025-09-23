d8d4094d501282e3682e3585859a3e67215e39a1
VoltNexis <voltedgebuilds@gmail.com>
2025-09-23 12:43:56 +0530

    Update upload.js

--- uploader/upload.js
+++ uploader/upload.js
@@ -1,4 +1,4 @@
-import { supabase } from '../js/supabase.js';
+import { supabase } from './js/supabase.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('course-upload-form');
